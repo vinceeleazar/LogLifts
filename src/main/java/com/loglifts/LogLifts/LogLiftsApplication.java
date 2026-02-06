69df5aca69f0c8330505de2bf945d526087dd53e
jay-vince eleazar <25145434+vinceeleazar@users.noreply.github.com>
2026-02-05 16:50:05 -0800

    Add: Set-up initial project structure and dependencies.
    Signed-off-by: jay-vince eleazar <25145434+vinceeleazar@users.noreply.github.com>

---
+++ src/main/java/com/loglifts/LogLifts/LogLiftsApplication.java
@@ -0,0 +1,13 @@
+package com.loglifts.LogLifts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogLiftsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LogLiftsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/loglifts/LogLifts/LogLiftsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loglifts.LogLifts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogLiftsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
